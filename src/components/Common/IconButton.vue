50abb24e236428bb29ba2a69ec932999fb8b158f
lucaseunchae <lucaskim.dev@gmail.com>
2023-09-25 22:59:29 +0900

    feat(common-component): add IconButton component

---
+++ src/components/Common/IconButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import BaseButton from '@/components/Base/BaseButton.vue'
+import type { BaseIconProps } from '@/components/Base/BaseIcon.vue'
+import BaseIcon from '@/components/Base/BaseIcon.vue'
+
+const props = defineProps<BaseIconProps>()
+</script>
+
+<template>
+  <BaseButton type="text">
+    <BaseIcon v-bind="props" />
+  </BaseButton>
+</template>
+
+<style scoped lang="scss"></style>
