46498b7fa52d64ee69313529c9f10a7ecb36cd39
lucaseunchae <lucaskim.dev@gmail.com>
2023-10-01 20:20:47 +0900

    feat: add TheSidebar component

---
+++ src/components/TheSidebar/TheSidebar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import BaseIcon from '@/components/Base/BaseIcon.vue'
+import TextBody2 from '@/components/Text/TextBody2.vue'
+import SidebarNavigation from '@/components/TheSidebar/SidebarNavigation.vue'
+import { ref } from 'vue'
+
+/* Constant */
+const navigations = ['전체 게시글', 'Vue', 'Javascript', 'Typescript'] as const
+type Navigations = (typeof navigations)[number]
+
+/* Local State */
+const activeNavigation = ref<Navigations>('전체 게시글')
+</script>
+
+<template>
+  <div class="the-sidebar">
+    <SidebarNavigation
+      v-for="navigation in navigations"
+      :key="navigation"
+      :name="navigation"
+      :active="navigation === activeNavigation"
+    >
+      <BaseIcon class="the-sidebar__navigation-symbol" name="circle" fill="1" />
+      <TextBody2 class="the-sidebar__navigation-name" weight="500">{{ navigation }}</TextBody2>
+    </SidebarNavigation>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.the-sidebar {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  height: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheSideBar from '@/components/TheSidebar/TheSidebar.vue'
+</script>
 
 <template>
-  <main>home</main>
+  <div class="home-view">
+    <TheSideBar />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.home-view {
+  display: flex;
+  gap: 40px;
+  height: 100%;
+}
+</style>
